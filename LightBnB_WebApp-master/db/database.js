72ded6bf4bd81d452061caa9fd8e454d917c0dd2
iDebonair <ofemiowolabi@gmail.com>
2023-07-10 11:39:26 +0000

    Updated the all reservations method to return real reservation as recieved from the database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -100,7 +100,28 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+query = `
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = $1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT $2;
+`
+const values = [guest_id, limit];
+
+return pool
+.query(query, values)
+.then((result) => {
+  const reservations = result.rows[0];
+  return reservations;
+})
+.catch((error) => {
+  console.error('Error retrieving user:', error);
+  throw error;
+});
 };
 
 /// Properties
